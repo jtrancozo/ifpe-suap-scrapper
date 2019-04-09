f65aeaee7dea2feef702d34334ffc6d981df2ed6
jonathantsilva <gthell1@gmail.com>
2019-04-09 12:13:58 -0300

    remove header

--- cmpa-latest.js
+++ cmpa-latest.js
@@ -1,13 +1,3 @@
-// ==UserScript==
-// @name         CMPA Etiqueta
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://suap.ifpe.edu.br/patrimonio/inventario_busca*
-// @grant        none
-// ==/UserScript==
-
 (function() {
     'use strict';
 
